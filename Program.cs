023cd4baa0fabea544d95468c6552f5053601e97
alirifatciftci <119420009+alirifatciftci@users.noreply.github.com>
2024-10-23 00:22:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using OGRENCİ_NOT_KAYIT_SİSTEMİ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OGRENCİ_NOT_KAYIT_SİSTEMİ
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmGiris());
+        }
+    }
+}
